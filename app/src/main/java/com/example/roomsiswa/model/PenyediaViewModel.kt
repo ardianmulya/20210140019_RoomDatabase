fa15bdde09139fe114e6c566f6c3730e1d8382fd
ardianmulya <ardian.mulya7@gmail.com>
2023-12-05 14:25:17 +0700

    Menambahkan PenyediaViewModel dengan Factory

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.roomsiswa.model
 
+import android.text.Spannable.Factory
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
