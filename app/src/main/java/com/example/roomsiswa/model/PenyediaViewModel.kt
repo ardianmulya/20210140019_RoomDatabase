67939e2e5d886f776e8ab0d632b0a4470f06ac14
ardianmulya <ardian.mulya7@gmail.com>
2023-12-05 14:27:42 +0700

    Menambahkan extension function aplikasiSiswa pada CreationExtras

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.roomsiswa.model
 
 import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomsiswa.AplikasiSiswa
@@ -8,10 +10,13 @@ import com.example.roomsiswa.AplikasiSiswa
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
         initializer {
-            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as AplikasiSiswa)
