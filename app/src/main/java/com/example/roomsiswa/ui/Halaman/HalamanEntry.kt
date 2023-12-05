adfc18e7034ae523c15fd9b726d76854b7915f46
ardianmulya <ardian.mulya7@gmail.com>
2023-12-05 14:35:07 +0700

    Menambahkan objek DestinasiEntry

---
+++ app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanEntry.kt
@@ -0,0 +1,10 @@
+package com.example.roomsiswa.ui.Halaman
+
+import com.example.roomsiswa.R
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
+
