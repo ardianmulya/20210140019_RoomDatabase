798e88faf2129389e47607e7590da21ed7a28d34
ardianmulya <ardian.mulya7@gmail.com>
2023-12-05 14:46:03 +0700

    membuat function FormInputSiswa

--- app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanEntry.kt
@@ -1,10 +1,28 @@
 package com.example.roomsiswa.ui.Halaman
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.roomsiswa.R
+import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry : DestinasiNavigasi{
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier,
+    onValueChange : (DetailSiswa) ->Unit = {},
+    enabled:Boolean = true
+){
+    Column (
+
+    ){
+
+    }
 }
 
