ffa03ec267e76da408606aa3f876689d7e2dc7cc
ardianmulya <ardian.mulya7@gmail.com>
2023-12-05 09:26:05 +0700

    fungsi validasi input

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -9,6 +9,12 @@ import com.example.roomsiswa.repositori.RepositoriSiswa
 class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private fun validasiInput (uiState:DetailSiswa = uiStateSiswa.detailSiswa):Boolean{
+        return  with(uiState){
+            nama.isNotBlank()&&alamat.isNotBlank()&&telepon.isNotBlank()
+        }
+    }
 }
 data class UIStateSiswa(
     val detailSiswa : DetailSiswa = DetailSiswa(),
